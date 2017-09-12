9498207a1bb7960cd253ec153a3edfb375891092
xhchrn <xhchrn@gmail.com>
2017-09-12 09:23:45 -0500

    compatible with python3

--- LAMP.py
+++ LAMP.py
@@ -21,7 +21,8 @@ tf.set_random_seed(1) # on the other hand, this is basically useless (see issue 
 from tools import problems,networks,train
 
 # Create the basic problem structure.
-prob = problems.bernoulli_gaussian_trial(kappa=None,M=250,N=500,L=1000,pnz=.1,SNR=40) #a Bernoulli-Gaussian x, noisily observed through a random matrix
+# prob = problems.bernoulli_gaussian_trial(kappa=None,M=250,N=500,L=1000,pnz=.1,SNR=40) #a Bernoulli-Gaussian x, noisily observed through a random matrix
+prob = problems.bernoulli_gaussian_trial(kappa=1000,M=250,N=500,L=1000,pnz=.1,SNR=40) #a Bernoulli-Gaussian x, noisily observed through a random matrix
 #prob = problems.random_access_problem(2) # 1 or 2 for compressive random access or massive MIMO
 
 # build a LAMP network to solve the problem and get the intermediate results so we can greedily extend and then refine(fine-tune)
@@ -31,4 +32,4 @@ layers = networks.build_LAMP(prob,T=6,shrink='bg',untied=False)
 training_stages = train.setup_training(layers,prob,trinit=1e-3,refinements=(.5,.1,.01) )
 
 # do the learning (takes a while)
-sess = train.do_training(training_stages,prob,'LAMP_bg_giid.npz')
+sess = train.do_training(training_stages,prob,'LAMP_bg_kappa1000.npz')
