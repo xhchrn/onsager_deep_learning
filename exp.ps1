4f66626cf273c3f9889753ff8a7a28c1fffca20a
xhchrn <xhchrn@gmail.com>
2017-10-04 13:44:27 -0500

    171004

---
+++ exp.ps1
@@ -0,0 +1,5 @@
+﻿For ($k=4; $k -le 40; ) {
+    python exp.py $k 0
+    If ($k -eq 4) { $k = 5 }
+    Else { $k = $k + 5 }
+}
--- train.ps1
+++ train.ps1
@@ -1,9 +1,7 @@
-For ($k=4; $k -le 100; ) {
-    For ($i=0; $i -le 4; $i++) {
-        python train_nets.py --kappa=$k --id=$i --model=LAMP --T=6
-        python train_nets.py --kappa=$k --id=$i --model=LVAMP --T=6
-        python train_nets.py --kappa=$k --id=$i --model=LISTA --T=6
-    }
+For ($k=4; $k -le 50; ) {
+    python train_nets.py --kappa=$k --id=0 --model=LAMP --T=6
+    python train_nets.py --kappa=$k --id=0 --model=LVAMP --T=6
+    python train_nets.py --kappa=$k --id=0 --model=LISTA --T=6
     If ($k -eq 4) { $k = 5 }
     Else { $k = $k + 5 }
 }
